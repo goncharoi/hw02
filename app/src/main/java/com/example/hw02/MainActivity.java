5f2d59a236bee882773c8fdd58b6b22a1526d4f9
goncharoi <s--e--t@yandex.ru>
2021-06-06 17:26:02 +0500

    Домашнее задание 5

--- app/src/main/java/com/example/hw02/MainActivity.java
+++ app/src/main/java/com/example/hw02/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.hw02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 import static com.example.hw02.Calculator.ERROR;
 
@@ -14,14 +17,32 @@ public class MainActivity extends AppCompatActivity {
         public static final String NUMBER = "Number";
     }
 
+    private  static class  ThemeKey {
+        private static final int APP_THEME = 0;
+        private static final int APP_THEME_GREEN = 1;
+    }
+
+    private static final String NAME_SHARED_PREFERENCE = "OPTIONS";
+    private static final String APP_THEME = "APP_THEME";
+    private static final String THEME_SWITCH_CHECKED = "THEME_SWITCH_CHECKED";
+
+    private SharedPreferences moSharedPref;
+
     private TextView moNumber;
+    private SwitchMaterial moSwitch;
     private Calculator moCalc;
     private boolean mvNoInput = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        moSharedPref = getSharedPreferences(NAME_SHARED_PREFERENCE, MODE_PRIVATE);
+        setTheme(getAppTheme(R.style.AppTheme));
         setContentView(R.layout.activity_main);
+
+        moSwitch = (SwitchMaterial)findViewById(R.id.theme_switch);
+        moSwitch.setChecked(moSharedPref.getBoolean(THEME_SWITCH_CHECKED,false));
 
         moNumber = findViewById(R.id.number);
         moCalc = new Calculator();
@@ -68,6 +89,21 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_9).setOnClickListener(v -> addCharToNumber(getString(R.string._9)));
         findViewById(R.id.button_dot).setOnClickListener(v -> addCharToNumber(getString(R.string._dot)));
 
+        //Переключение темы
+        findViewById(R.id.theme_switch).setOnClickListener(v ->  {
+                // сохраним настройки
+                if (moSwitch.isChecked()){
+                    setAppTheme(ThemeKey.APP_THEME_GREEN);
+                } else {
+                    setAppTheme(ThemeKey.APP_THEME);
+                }
+                SharedPreferences.Editor editor = moSharedPref.edit();
+                editor.putBoolean(THEME_SWITCH_CHECKED,moSwitch.isChecked());
+                editor.apply();
+
+                // пересоздадим активити, чтобы тема применилась
+                recreate();
+        });
     }
 
     private void addCharToNumber(String ivStr){
@@ -103,6 +139,31 @@ public class MainActivity extends AppCompatActivity {
         super.onRestoreInstanceState(instanceState);
         moCalc = (Calculator) instanceState.getSerializable(Key.CALCULATOR);
         moNumber.setText(instanceState.getString(Key.NUMBER));
+    }
+
+    private int getAppTheme(int ivCodeStyle) {
+        return codeStyleToStyleId(getCodeStyle(ivCodeStyle));
+    }
+
+    // Чтение настроек, параметр «тема»
+    private int getCodeStyle(int ivCodeStyle){
+        return moSharedPref.getInt(APP_THEME, ivCodeStyle);
+    }
+
+    // Сохранение настроек
+    private void setAppTheme(int ivCodeStyle) {
+        SharedPreferences.Editor editor = moSharedPref.edit();
+        editor.putInt(APP_THEME, ivCodeStyle);
+        editor.apply();
+    }
+
+    private int codeStyleToStyleId(int ivCodeStyle){
+        switch(ivCodeStyle){
+            case ThemeKey.APP_THEME_GREEN:
+                return R.style.AppThemeGreen;
+            default:
+                return R.style.AppTheme;
+        }
     }
 
 }
